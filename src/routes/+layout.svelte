057a60ed9d8506ef65a08dc5d526af1dfe9763ab
Shaun Chong <data-miner00@users.noreply.github.com>
2025-12-04 14:20:40 +0800

    feat: Setup toast notification

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,7 @@
 	import './layout.css';
 	import favicon from '$lib/assets/favicon.svg';
 	import Header from '$lib/components/custom/shared/Header.svelte';
+	import { Toaster } from '$lib/components/ui/sonner';
 
 	let { children } = $props();
 </script>
@@ -11,6 +12,8 @@
 	<title>Webutils</title>
 	<meta name="description" content="A collection of web utilities built with SvelteKit." />
 </svelte:head>
+
+<Toaster position="top-center" />
 
 <Header />
 
