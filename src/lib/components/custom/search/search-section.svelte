7cb40e81ebc6bff8afb6949c4b01f8c3f0009370
Shaun Chong <data-miner00@users.noreply.github.com>
2025-12-04 11:26:37 +0800

    wip(fix): Weird compatibility issues

--- src/lib/components/custom/search/search-section.svelte
+++ src/lib/components/custom/search/search-section.svelte
@@ -1,9 +1,8 @@
 <script lang="ts">
-	import * as DropdownMenu from '$lib/components/ui/dropdown-menu/index.js';
-	import * as InputGroup from '$lib/components/ui/input-group/index.js';
+	import * as DropdownMenu from '$lib/components/ui/dropdown-menu';
+	import * as InputGroup from '$lib/components/ui/input-group';
 	import ChevronDownIcon from '@lucide/svelte/icons/chevron-down';
-	import * as Tooltip from '$lib/components/ui/tooltip/index.js';
-	import Search from '@lucide/svelte/icons/search';
+	import Input from '$lib/components/ui/input/input.svelte';
 
 	type SearchEngine = {
 		faviconUrl: string;
@@ -45,63 +44,38 @@
 	function performSearch(e: KeyboardEvent) {
 		if (e.key !== 'Enter') return;
 		window.open(searchUrl, '_blank');
+		query = '';
 	}
 </script>
 
 <div class="flex items-center">
-	<InputGroup.Root>
-		<InputGroup.Input
-			placeholder="Search the internet or type a URL"
-			bind:value={query}
-			onkeydown={performSearch}
-		/>
-		<InputGroup.Addon align="inline-end">
-			<DropdownMenu.Root>
-				<DropdownMenu.Trigger>
-					{#snippet child({ props })}
-						<InputGroup.Button {...props} variant="ghost" class="!pe-1.5 text-xs">
-							{currentSearchEngine.name}
-							<ChevronDownIcon class="size-3" />
-						</InputGroup.Button>
-					{/snippet}
-				</DropdownMenu.Trigger>
-				<DropdownMenu.Content align="end" class="[--radius:0.95rem]">
-					<DropdownMenu.RadioGroup bind:value={currentSearchEngine2}>
-						{#each searchEngines as engine}
-							<DropdownMenu.RadioItem value={engine.id}>
-								{engine.name}
-							</DropdownMenu.RadioItem>
-						{/each}
-					</DropdownMenu.RadioGroup>
-				</DropdownMenu.Content>
-			</DropdownMenu.Root>
-		</InputGroup.Addon>
-		<InputGroup.Addon align="inline-start">
-			<Tooltip.Provider>
-				<Tooltip.Root>
-					<Tooltip.Trigger>
-						{#snippet child({ props })}
-							<InputGroup.Addon>
-								<InputGroup.Button {...props} variant="ghost" aria-label="Help" size="icon-xs">
-									<Search />
-								</InputGroup.Button>
-							</InputGroup.Addon>
-						{/snippet}
-					</Tooltip.Trigger>
-					<Tooltip.Content>
-						<p>Search the web using {currentSearchEngine.name}</p>
-					</Tooltip.Content>
-				</Tooltip.Root>
-			</Tooltip.Provider>
-		</InputGroup.Addon>
-	</InputGroup.Root>
-</div>
+	<Input
+		placeholder="Search the internet or type a URL"
+		bind:value={query}
+		onkeydown={performSearch}
+	/>
+	<DropdownMenu.Root>
+		<DropdownMenu.Trigger>
+			{#snippet child({ props })}
+				<InputGroup.Button {...props} variant="ghost" class="!pe-1.5 text-xs">
+					{currentSearchEngine.name}
+					<ChevronDownIcon class="size-3" />
+				</InputGroup.Button>
+			{/snippet}
+		</DropdownMenu.Trigger>
 
-<!-- <Tooltip.Provider>
-	<Tooltip.Root>
-		<Tooltip.Trigger>Hover</Tooltip.Trigger>
-		<Tooltip.Content>
-			<p>Add to library</p>
-		</Tooltip.Content>
-	</Tooltip.Root>
-</Tooltip.Provider> -->
+		<DropdownMenu.Content align="end" class="[--radius:0.95rem]">
+			<DropdownMenu.Group>
+				<DropdownMenu.Label>Search Engine</DropdownMenu.Label>
+				<DropdownMenu.Separator />
+				<DropdownMenu.RadioGroup bind:value={currentSearchEngine2}>
+					{#each searchEngines as engine}
+						<DropdownMenu.RadioItem value={engine.id}>
+							{engine.name}
+						</DropdownMenu.RadioItem>
+					{/each}
+				</DropdownMenu.RadioGroup>
+			</DropdownMenu.Group>
+		</DropdownMenu.Content>
+	</DropdownMenu.Root>
+</div>
