5037280be42d92b9ae83bb476a11a331a891bfd1
Shaun Chong <data-miner00@users.noreply.github.com>
2025-12-04 18:25:22 +0800

    feat: Add weather report

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,39 @@
 <script>
 	import SearchSection from '$lib/components/custom/search/search-section.svelte';
+
+	async function fetchTodayWeather() {
+		const now = new Intl.DateTimeFormat('en-US').format(new Date());
+		const todayFromLocal = localStorage.getItem(now);
+
+		if (todayFromLocal) {
+			return todayFromLocal;
+		}
+
+		const res = await fetch('https://wttr.in');
+		const html = await res.text();
+
+		localStorage.setItem(now, html);
+
+		return html;
+	}
 </script>
 
 <h1 class="text-2xl font-bold mb-2">Web Search</h1>
 
-<p class="mb-4">Search the internet now!</p>
+<p class="mb-4">
+	Search the internet now!Search the internet now!Search the internet now!Search the internet now!
+</p>
 
 <SearchSection />
 
 <!-- <img src="x" onerror={alert('XSS Attack!')} /> -->
+
+<h1 class="text-2xl font-bold mb-2 mt-5">Weather</h1>
+
+{#await fetchTodayWeather()}
+	<p>Fetching weather info...</p>
+{:then content}
+	{@html content}
+{:catch error}
+	<p class="text-red-500">Error fetching weather info: {error.message}</p>
+{/await}
