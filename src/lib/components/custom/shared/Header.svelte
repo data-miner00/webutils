0dd03dcdce2ed2eb3ef11c7b691e9a0931eb4b0d
Shaun Chong <data-miner00@users.noreply.github.com>
2026-01-03 17:50:06 +0800

    feat: Added internet connection status

--- src/lib/components/custom/shared/Header.svelte
+++ src/lib/components/custom/shared/Header.svelte
@@ -5,11 +5,13 @@
 	import { onMount } from 'svelte';
 	import { currentLocalTimeInfo } from '$lib/core/clock-utils';
 	import SearchSection from '../search/search-section.svelte';
+	import { Wifi, WifiOff } from '@lucide/svelte';
 
 	let username = $state('');
 	let email = $state('');
 	let avatarUrl = $state('');
 	let currentTime = $state(currentLocalTimeInfo(true));
+	let isOnline = $state(true);
 
 	onMount(() => {
 		const savedUsername = localStorage.getItem('settings_username');
@@ -26,6 +28,9 @@
 
 		return () => clearInterval(interval);
 	});
+
+	window.addEventListener('online', () => (isOnline = true));
+	window.addEventListener('offline', () => (isOnline = false));
 </script>
 
 <header class="flex w-full items-center justify-between bg-background p-4">
@@ -114,6 +119,13 @@
 	<SearchSection />
 
 	<div class="flex gap-4 items-center">
+		<div title={isOnline ? 'Connected to internet' : 'Not connected'}>
+			{#if isOnline}
+				<Wifi class="text-green-500" />
+			{:else}
+				<WifiOff class="text-red-500" />
+			{/if}
+		</div>
 		<time datetime={currentTime.date} class="text-right">
 			<div class="font-bold">
 				{currentTime.date}
