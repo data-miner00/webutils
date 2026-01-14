9695f4706d2f6e5142cdc711a3cf6bc218abb9be
Shaun Chong <data-miner00@users.noreply.github.com>
2026-01-14 18:29:53 +0800

    feat: Revamp notes UI

--- src/routes/manage/notes/+page.svelte
+++ src/routes/manage/notes/+page.svelte
@@ -1,161 +1,60 @@
 <script lang="ts">
-	import * as Empty from '$lib/components/ui/empty/index.js';
 	import { Button } from '$lib/components/ui/button/index.js';
-	import FolderCodeIcon from '@lucide/svelte/icons/folder-code';
-	import ArrowUpRightIcon from '@lucide/svelte/icons/arrow-up-right';
-	import * as Dialog from '$lib/components/ui/dialog';
-	import Input from '$lib/components/ui/input/input.svelte';
 	import { onMount } from 'svelte';
-	import Textarea from '$lib/components/ui/textarea/textarea.svelte';
-	import { EllipsisVertical, Trash } from '@lucide/svelte';
+	import { EllipsisVertical, FileText } from '@lucide/svelte';
 	import * as InputGroup from '$lib/components/ui/input-group/index.js';
 	import { Plus, SearchIcon } from '@lucide/svelte';
-
-	let isDialogOpen = $state(false);
-
-	// Migrate to use indexDB
-	type Note = {
-		id: string;
-		title: string;
-		content: string;
-		createdAt: string;
-	};
+	import CodeEditor from '$lib/components/custom/code-editor/code-editor.svelte';
+	import { type Note, notesRepository } from '$lib/core/notes';
 
 	let notes = $state<Note[]>([]);
+	let currentId = $state<string | null>(null);
 
-	onMount(() => {
-		const existingNotes = localStorage.getItem('notes');
+	onMount(async () => {
+		if (!notes || notes.length === 0) {
+			const defaultNote: Note = {
+				id: crypto.randomUUID(),
+				title: 'Welcome Note',
+				content:
+					'# Welcome to the Notes Manager!\n\nThis is your first note. You can edit or delete it, and create new notes using the "Create Note" button above.\n\nHappy note-taking!',
+				createdAt: new Date().toISOString()
+			};
 
-		if (existingNotes) {
-			notes = JSON.parse(existingNotes);
+			const secondNote: Note = {
+				id: crypto.randomUUID(),
+				title: 'Untitled Note',
+				content: 'Hello world!',
+				createdAt: new Date().toISOString()
+			};
+			notes = [defaultNote, secondNote];
+			currentId = defaultNote.id;
+
+			return;
 		}
 	});
 
-	let selectedNote = $derived<Note | undefined>(
-		notes.find((note, index) => index === currentIndex)
-	);
-
-	let currentIndex = $state(0);
-
-	function addNewNote() {
-		const newNote: Note = {
-			id: crypto.randomUUID(),
-			title: 'Untitled Note',
-			content: '',
-			createdAt: new Date().toISOString()
-		};
-		notes = [...notes, newNote];
-
-		localStorage.setItem('notes', JSON.stringify(notes));
-	}
-
-	function updateNote() {
-		const { id, title, content } = selectedNote!;
-
-		notes = notes.map((note) => (note.id === id ? { ...note, title, content } : note));
-
-		localStorage.setItem('notes', JSON.stringify(notes));
-
-		isDialogOpen = false;
-	}
-
-	function deleteNote(id: string) {
-		notes = notes.filter((note) => note.id !== id);
-		localStorage.setItem('notes', JSON.stringify(notes));
-	}
+	let selectedNote = $derived<Note | undefined>(notes.find((note) => note.id === currentId));
 
 	let searchQuery = $state('');
 
 	let filteredNotes = $derived(
-		notes.filter((x) => x.title.toLowerCase().includes(searchQuery.toLowerCase()))
+		notes
+			.filter((x) => x.title.toLowerCase().includes(searchQuery.toLowerCase()))
+			.sort((a, b) => b.createdAt.localeCompare(a.createdAt))
 	);
 </script>
 
-<h1 class="text-2xl font-bold mb-2">My Notes</h1>
-
-<div class="flex gap-4 items-center mb-5">
-	<InputGroup.Root>
-		<InputGroup.Input bind:value={searchQuery} placeholder="Search..." />
-		<InputGroup.Addon>
-			<SearchIcon />
-		</InputGroup.Addon>
-		{#if searchQuery}
-			<InputGroup.Addon align="inline-end">
-				<InputGroup.Button>{filteredNotes.length} results</InputGroup.Button>
-			</InputGroup.Addon>
-		{/if}
-	</InputGroup.Root>
-
-	<div class="flex gap-2">
-		<Button onclick={addNewNote}>
-			<Plus /> Create Note
-		</Button>
-		<Button variant="outline" size="icon">
-			<EllipsisVertical />
-			<!-- Clear All -->
-			<!-- Export to Json -->
-		</Button>
-	</div>
-</div>
-
-{#if notes.length > 0}
-	<div class="flex gap-4 flex-wrap">
-		{#each filteredNotes as note (note.id)}
-			<div class="border border-solid border-gray-200 p-2 rounded">
-				<p>{note.title}</p>
-				<p class="text-sm text-gray-500">{new Date(note.createdAt).toLocaleDateString()}</p>
-				<button
-					class="text-blue-500 underline"
-					onclick={() => {
-						currentIndex = notes.indexOf(note);
-						isDialogOpen = true;
-					}}>Edit</button
-				>
-				<button onclick={() => deleteNote(note.id)}>
-					<Trash />
-				</button>
-			</div>
+<div class="mb-4">
+	<div class="border border-solid border-gray-300 flex items-center border-b-0 bg-gray-50">
+		{#each notes as note}
+			<button
+				class={`w-[150px] px-3 py-2 flex gap-2 items-center border-l border-gray-300 border-solid first-of-type:border-l-0 text-sm relative ${currentId === note.id ? 'border-t-black border-t-2 bg-white after:absolute after:left-0 after:right-0 after:-bottom-1 after:h-1 after:bg-white' : 'bg-gray-50'}`}
+				onclick={() => (currentId = note.id)}
+			>
+				<FileText size="16" />
+				<div class="truncate">{note.title}</div>
+			</button>
 		{/each}
 	</div>
-{:else}
-	<Empty.Root>
-		<Empty.Header>
-			<Empty.Media variant="icon">
-				<FolderCodeIcon />
-			</Empty.Media>
-			<Empty.Title>No Notes Yet</Empty.Title>
-			<Empty.Description>
-				You haven't created any notes yet. Get started by creating your first note.
-			</Empty.Description>
-		</Empty.Header>
-		<Empty.Content>
-			<div class="flex gap-2">
-				<Button onclick={addNewNote}>Create Note</Button>
-				<Button variant="outline">Import Note</Button>
-			</div>
-		</Empty.Content>
-		<Button variant="link" class="text-muted-foreground" size="sm">
-			<a href="#/">
-				Learn More <ArrowUpRightIcon class="inline" />
-			</a>
-		</Button>
-	</Empty.Root>
-{/if}
-
-{#if selectedNote}
-	<Dialog.Root open={isDialogOpen} onOpenChange={(isOpen) => (isDialogOpen = isOpen)}>
-		<Dialog.Content>
-			<Dialog.Header>
-				<Dialog.Title>
-					<Input bind:value={selectedNote.title} placeholder="Note Title" />
-				</Dialog.Title>
-			</Dialog.Header>
-
-			<Textarea bind:value={selectedNote.content} placeholder="Enter content here..."></Textarea>
-
-			<Dialog.Footer>
-				<Button type="submit" onclick={updateNote}>Save changes</Button>
-			</Dialog.Footer>
-		</Dialog.Content>
-	</Dialog.Root>
-{/if}
+	<CodeEditor language="text" value={selectedNote?.content || ''} class="h-full" />
+</div>
