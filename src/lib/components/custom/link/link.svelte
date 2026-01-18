5052cd321678043544eeda17c89ec0a9fdfb807c
Shaun Chong <data-miner00@users.noreply.github.com>
2026-01-18 23:06:37 +0800

    feat: Fetch favicon via google api

--- src/lib/components/custom/link/link.svelte
+++ src/lib/components/custom/link/link.svelte
@@ -8,6 +8,8 @@
 	};
 
 	let { url, title, language }: Props = $props();
+
+	let domain = $derived(extractDomain(url));
 </script>
 
 <a
@@ -16,11 +18,17 @@
 	target="_blank"
 >
 	<div>
-		<div class="w-6 h-6 rounded-full bg-gray-300"></div>
+		<div class="w-6 h-6 rounded-full flex items-center">
+			<img
+				class="block"
+				src={`https://www.google.com/s2/favicons?sz=32&domain_url=${url}`}
+				alt="Favicon"
+			/>
+		</div>
 	</div>
 	<div class="ml-4 w-[200px]">
 		<p>{title}</p>
-		<p class="text-xs text-muted-foreground lowercase">{extractDomain(url)}</p>
+		<p class="text-xs text-muted-foreground lowercase">{domain}</p>
 	</div>
 	<div
 		class="absolute top-1/2 right-4 -translate-y-1/2 border border-solid border-gray-500 text-gray-500 px-1 rounded uppercase text-xs"
