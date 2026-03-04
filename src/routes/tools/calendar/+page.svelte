4eb01bf6a1410c48d483e95b3492909f62c333c9
Shaun Chong <data-miner00@users.noreply.github.com>
2026-03-04 23:24:30 +0800

    wip: Add 2 wip selection

--- src/routes/tools/calendar/+page.svelte
+++ src/routes/tools/calendar/+page.svelte
@@ -2,6 +2,8 @@
 	import Calendar from '$lib/components/ui/calendar/calendar.svelte';
 	import { CalendarDate } from '@internationalized/date';
 	import { onMount } from 'svelte';
+	import * as NativeSelect from '$lib/components/ui/native-select/index.js';
+	import Label from '$lib/components/ui/label/label.svelte';
 
 	const now = new Date();
 	let value = $state<CalendarDate | undefined>(new CalendarDate(now.getFullYear(), 1, 1));
@@ -9,7 +11,32 @@
 	onMount(() => {
 		value = new CalendarDate(now.getFullYear(), now.getMonth() + 1, now.getDate());
 	});
+
+	const years = Array.from({ length: 100 }, (_, i) => now.getFullYear() - 50 + i);
+	let selectedYear = $state(now.getFullYear());
+	let selectedDisplayLayout = $state('year');
+
+	const displayLayout = [
+		{ label: 'Year', value: 'year' },
+		{ label: 'Month', value: 'month' },
+		{ label: 'Week', value: 'week' },
+		{ label: 'Work Week', value: 'work-week' }
+	];
 </script>
+
+<Label class="mb-2">Select a year</Label>
+<NativeSelect.Root bind:value={selectedYear}>
+	{#each years as year}
+		<NativeSelect.Option value={year}>{year}</NativeSelect.Option>
+	{/each}
+</NativeSelect.Root>
+
+<Label class="my-2">Display</Label>
+<NativeSelect.Root bind:value={selectedDisplayLayout}>
+	{#each displayLayout as layout}
+		<NativeSelect.Option value={layout.value}>{layout.label}</NativeSelect.Option>
+	{/each}
+</NativeSelect.Root>
 
 <div>
 	<Calendar
