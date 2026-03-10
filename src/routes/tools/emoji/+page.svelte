204b03c6c6be559b176ba76e9f7ff9fd2b37607b
Shaun Chong <data-miner00@users.noreply.github.com>
2026-03-10 18:38:35 +0800

    feat: Implemented category for emoji

--- src/routes/tools/emoji/+page.svelte
+++ src/routes/tools/emoji/+page.svelte
@@ -1,14 +1,17 @@
 <script lang="ts">
-	import { emojis } from '$lib/core/emoji';
-	import { clickToCopy } from '$lib/core/copy-to-clipboard';
-	import References from '$lib/components/custom/references/references.svelte';
-	import * as InputGroup from '$lib/components/ui/input-group/index.js';
 	import { X } from '@lucide/svelte';
 
+	import References from '$lib/components/custom/references/references.svelte';
+	import { Button } from '$lib/components/ui/button';
+	import * as InputGroup from '$lib/components/ui/input-group/index.js';
+	import { clickToCopy } from '$lib/core/copy-to-clipboard';
+	import { emojis } from '$lib/core/emoji';
+
 	let searchTerm = $state('');
+	let selectedCategory = $state('All');
 
 	let filteredEmojis = $derived(
-		emojis.filter(
+		filterByCategory(selectedCategory).filter(
 			(e) =>
 				e.emoji.includes(searchTerm) ||
 				e.category.includes(searchTerm) ||
@@ -16,20 +19,54 @@
 		)
 	);
 
-	// Todo: implement category and history mechanism
+	let categories = $derived(
+		Array.from(new Set(emojis.map((emoji) => emoji.category))).filter((cat) => cat)
+	);
+
+	function filterByCategory(category: string) {
+		if (category === 'All') {
+			return emojis;
+		}
+		return emojis.filter((emoji) => emoji.category === category);
+	}
+
+	// Todo: implement history mechanism
 </script>
 
 <InputGroup.Root class="mb-4">
 	<InputGroup.Input placeholder="Search emojis..." name="search" required bind:value={searchTerm} />
-	<InputGroup.Addon align="inline-end">
-		<InputGroup.Button variant="ghost" onclick={() => (searchTerm = '')}><X /></InputGroup.Button>
-	</InputGroup.Addon>
+
+	{#if searchTerm}
+		<InputGroup.Addon align="inline-end">
+			<InputGroup.Button variant="ghost" onclick={() => (searchTerm = '')}><X /></InputGroup.Button>
+		</InputGroup.Addon>
+	{/if}
 </InputGroup.Root>
 
-<div class="grid grid-cols-8 sm:grid-cols-10 md:grid-cols-12 lg:grid-cols-16 gap-2">
+<div
+	class="mb-6 flex w-full items-center gap-2 overflow-x-scroll [-ms-overflow-style:none] [scrollbar-width:none] [&::-webkit-scrollbar]:hidden"
+>
+	<Button
+		size="sm"
+		variant={selectedCategory === 'All' ? 'default' : 'secondary'}
+		onclick={() => (selectedCategory = 'All')}
+		class="cursor-pointer">All</Button
+	>
+
+	{#each categories as category}
+		<Button
+			size="sm"
+			variant={selectedCategory === category ? 'default' : 'secondary'}
+			onclick={() => (selectedCategory = category)}
+			class="cursor-pointer">{category}</Button
+		>
+	{/each}
+</div>
+
+<div class="grid grid-cols-8 gap-2 sm:grid-cols-10 md:grid-cols-12 lg:grid-cols-16">
 	{#each filteredEmojis as emoji}
 		<button
-			class="flex justify-center items-center p-2 bg-gray-100 rounded-lg cursor-pointer hover:bg-gray-200"
+			class="flex cursor-pointer items-center justify-center rounded-lg bg-gray-100 p-2 hover:bg-gray-200"
 			{@attach (node) => clickToCopy(node)}
 			title={emoji.metadata}
 		>
@@ -43,6 +80,10 @@
 		{
 			title: 'Emojibase',
 			url: 'https://emojibase.dev/'
+		},
+		{
+			title: 'Emojipedia',
+			url: 'https://emojipedia.org/'
 		}
 	]}
 />
