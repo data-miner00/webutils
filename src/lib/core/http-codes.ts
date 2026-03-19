9737dcbb1cfd7e0d12d50aef96d1fd6d2ddfb965
Shaun Chong <data-miner00@users.noreply.github.com>
2026-03-19 22:18:05 +0800

    fix: Cast to string to enable search

--- src/lib/core/http-codes.ts
+++ src/lib/core/http-codes.ts
@@ -1,11 +1,11 @@
 import codes from './http-codes.json';
 
 export type HttpCode = {
-	value: number;
+	value: string;
 	description: string;
 	meaning: string;
 	deprecated: boolean;
 	rfc: string;
 };
 
-export const httpCodes: HttpCode[] = codes;
+export const httpCodes: HttpCode[] = codes.map((x) => ({ ...x, value: String(x.value) }));
