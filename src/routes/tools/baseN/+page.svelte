b2064d3d2b663728b98da5790c0b614106b21b3b
Shaun Chong <data-miner00@users.noreply.github.com>
2026-05-03 16:30:02 +0800

    refactor: Improved baseN interface

--- src/routes/tools/baseN/+page.svelte
+++ src/routes/tools/baseN/+page.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	import { Album, Clipboard, Copy, EllipsisVertical, X } from '@lucide/svelte';
+	import { Album, ArrowBigDown, Clipboard, Copy, EllipsisVertical, X } from '@lucide/svelte';
 
 	import * as ButtonGroup from '$lib/components/ui/button-group/index.js';
 	import { Button } from '$lib/components/ui/button/index.js';
@@ -69,11 +69,15 @@
 	}
 </script>
 
-<div class="grid h-full grid-cols-2 gap-4 px-4 py-6">
-	<section class="flex flex-1 flex-col overflow-hidden">
-		<header class="mb-6 flex justify-between">
-			<h1 class="block text-xl font-bold">Base Number Conversion</h1>
-			<div class="flex items-center gap-4">
+<div class="mx-auto mt-10 max-w-xl">
+	<header class="mb-20">
+		<h1 class="block text-center text-xl font-bold">Base Number Conversion</h1>
+		<p class="text-muted-foreground text-center">Convert numbers between different bases.</p>
+	</header>
+
+	<div class="flex flex-col gap-6">
+		<div class="flex justify-end">
+			<div class="flex items-center gap-2">
 				<ButtonGroup.Root>
 					<ButtonGroup.Root>
 						<Button variant="outline" onclick={loadBinaryExample}>Binary Example</Button>
@@ -109,7 +113,7 @@
 					</ButtonGroup.Root>
 				</ButtonGroup.Root>
 			</div>
-		</header>
+		</div>
 		<div>
 			<Label for="base" class="mb-2">Base</Label>
 			<Select.Root type="single" name="baseType" bind:value={mode}>
@@ -145,94 +149,102 @@
 				{/if}
 			</InputGroup.Root>
 		</div>
-	</section>
 
-	<section class="flex flex-1 flex-col overflow-hidden">
-		<header class="mb-6 flex justify-between">
-			<h2 class="block text-xl font-bold">Output</h2>
-
-			<ButtonGroup.Root>
-				<Button variant="outline" onclick={copyOutput}><Clipboard /> Copy as JSON</Button>
-			</ButtonGroup.Root>
-		</header>
-
-		<div>
-			<Label for="binaryOutput" class="mb-2">Binary</Label>
-
-			<InputGroup.Root class="mb-6">
-				<InputGroup.Input value={output.decimalToBinary} readonly />
-				<InputGroup.Addon align="inline-end">
-					<InputGroup.Button
-						aria-label="Copy"
-						title="Copy"
-						size="icon-xs"
-						onclick={() => copyText(output.decimalToBinary)}
-					>
-						<Copy />
-					</InputGroup.Button>
-				</InputGroup.Addon>
-			</InputGroup.Root>
-
-			<Label for="octalOutput" class="mb-2">Octal</Label>
-
-			<InputGroup.Root class="mb-6">
-				<InputGroup.Input value={output.decimalToOctal} readonly />
-				<InputGroup.Addon align="inline-end">
-					<InputGroup.Button
-						aria-label="Copy"
-						title="Copy"
-						size="icon-xs"
-						onclick={() => copyText(output.decimalToOctal)}
-					>
-						<Copy />
-					</InputGroup.Button>
-				</InputGroup.Addon>
-			</InputGroup.Root>
-
-			<Label for="decimalOutput" class="mb-2">Decimal</Label>
-			<InputGroup.Root class="mb-6">
-				<InputGroup.Input value={decimalInput} readonly />
-				<InputGroup.Addon align="inline-end">
-					<InputGroup.Button
-						aria-label="Copy"
-						title="Copy"
-						size="icon-xs"
-						onclick={() => copyText(decimalInput.toString())}
-					>
-						<Copy />
-					</InputGroup.Button>
-				</InputGroup.Addon>
-			</InputGroup.Root>
-
-			<Label for="hexadecimalOutput" class="mb-2">Hexadecimal</Label>
-			<InputGroup.Root class="mb-6">
-				<InputGroup.Input value={output.decimalToHexadecimal} readonly />
-				<InputGroup.Addon align="inline-end">
-					<InputGroup.Button
-						aria-label="Copy"
-						title="Copy"
-						size="icon-xs"
-						onclick={() => copyText(output.decimalToHexadecimal)}
-					>
-						<Copy />
-					</InputGroup.Button>
-				</InputGroup.Addon>
-			</InputGroup.Root>
-
-			<Label for="base32Output" class="mb-2">Base32</Label>
-			<InputGroup.Root class="mb-6">
-				<InputGroup.Input value={output.decimalToBase32} readonly />
-				<InputGroup.Addon align="inline-end">
-					<InputGroup.Button
-						aria-label="Copy"
-						title="Copy"
-						size="icon-xs"
-						onclick={() => copyText(output.decimalToBase32)}
-					>
-						<Copy />
-					</InputGroup.Button>
-				</InputGroup.Addon>
-			</InputGroup.Root>
+		<div class="mx-auto">
+			<ArrowBigDown />
 		</div>
-	</section>
+
+		<div class="flex grid-cols-2 flex-col gap-2 md:grid">
+			<div>
+				<Label for="binaryOutput" class="mb-2">Binary</Label>
+
+				<InputGroup.Root class="mb-6">
+					<InputGroup.Input value={output.decimalToBinary} readonly />
+					<InputGroup.Addon align="inline-end">
+						<InputGroup.Button
+							aria-label="Copy"
+							title="Copy"
+							size="icon-xs"
+							onclick={() => copyText(output.decimalToBinary)}
+						>
+							<Copy />
+						</InputGroup.Button>
+					</InputGroup.Addon>
+				</InputGroup.Root>
+			</div>
+
+			<div>
+				<Label for="octalOutput" class="mb-2">Octal</Label>
+
+				<InputGroup.Root class="mb-6">
+					<InputGroup.Input value={output.decimalToOctal} readonly />
+					<InputGroup.Addon align="inline-end">
+						<InputGroup.Button
+							aria-label="Copy"
+							title="Copy"
+							size="icon-xs"
+							onclick={() => copyText(output.decimalToOctal)}
+						>
+							<Copy />
+						</InputGroup.Button>
+					</InputGroup.Addon>
+				</InputGroup.Root>
+			</div>
+
+			<div>
+				<Label for="decimalOutput" class="mb-2">Decimal</Label>
+				<InputGroup.Root class="mb-6">
+					<InputGroup.Input value={decimalInput} readonly />
+					<InputGroup.Addon align="inline-end">
+						<InputGroup.Button
+							aria-label="Copy"
+							title="Copy"
+							size="icon-xs"
+							onclick={() => copyText(decimalInput.toString())}
+						>
+							<Copy />
+						</InputGroup.Button>
+					</InputGroup.Addon>
+				</InputGroup.Root>
+			</div>
+
+			<div>
+				<Label for="hexadecimalOutput" class="mb-2">Hexadecimal</Label>
+				<InputGroup.Root class="mb-6">
+					<InputGroup.Input value={output.decimalToHexadecimal} readonly />
+					<InputGroup.Addon align="inline-end">
+						<InputGroup.Button
+							aria-label="Copy"
+							title="Copy"
+							size="icon-xs"
+							onclick={() => copyText(output.decimalToHexadecimal)}
+						>
+							<Copy />
+						</InputGroup.Button>
+					</InputGroup.Addon>
+				</InputGroup.Root>
+			</div>
+
+			<div>
+				<Label for="base32Output" class="mb-2">Base32</Label>
+				<InputGroup.Root class="mb-6">
+					<InputGroup.Input value={output.decimalToBase32} readonly />
+					<InputGroup.Addon align="inline-end">
+						<InputGroup.Button
+							aria-label="Copy"
+							title="Copy"
+							size="icon-xs"
+							onclick={() => copyText(output.decimalToBase32)}
+						>
+							<Copy />
+						</InputGroup.Button>
+					</InputGroup.Addon>
+				</InputGroup.Root>
+			</div>
+		</div>
+
+		<ButtonGroup.Root class="ml-auto">
+			<Button variant="outline" onclick={copyOutput}><Clipboard /> Copy as JSON</Button>
+		</ButtonGroup.Root>
+	</div>
 </div>
