f72a60c20feb22067d5ffb58df81c92b40c7b9e3
Shaun Chong <data-miner00@users.noreply.github.com>
2026-06-20 17:05:38 +0800

    feat: Link count setting

--- src/routes/settings/home/+page.svelte
+++ src/routes/settings/home/+page.svelte
@@ -1,17 +1,32 @@
 <script lang="ts">
+	import { toast } from 'svelte-sonner';
+
+	import { Button } from '$lib/components/ui/button';
+	import { Input } from '$lib/components/ui/input';
 	import * as Label from '$lib/components/ui/label/index.js';
 	import * as Select from '$lib/components/ui/select/index.js';
 	import Separator from '$lib/components/ui/separator/separator.svelte';
 	import { Switch } from '$lib/components/ui/switch/index.js';
 	import { searchEngines } from '$lib/core/search-engine';
 
-	let isEnableBetaFeatures = $state(localStorage.getItem('betaFeaturesEnabled') === 'true');
+	const isLinkEnabledStorageKey = 'homepageLink';
+	const isWebSearchEnabledStorageKey = 'homepageWebSearch';
+	const linkCountStorageKey = 'homepageLinkCount';
 
-	$effect(() => {
-		localStorage.setItem('searchEngine', value);
-	});
+	let isLinkEnabled = $state(localStorage.getItem(isLinkEnabledStorageKey) === 'true');
+	let isWebSearchEnabled = $state(localStorage.getItem(isWebSearchEnabledStorageKey) === 'true');
+	let linkCount = $state(Number(localStorage.getItem(linkCountStorageKey) || '8'));
 
-	let value = $state('');
+	let searchEngineInput = $state('');
+
+	function saveChanges() {
+		localStorage.setItem('searchEngine', searchEngineInput);
+		localStorage.setItem(isLinkEnabledStorageKey, JSON.stringify(isLinkEnabled));
+		localStorage.setItem(isWebSearchEnabledStorageKey, JSON.stringify(isWebSearchEnabled));
+		localStorage.setItem(linkCountStorageKey, JSON.stringify(linkCount));
+
+		toast.success('Settings updated successfully');
+	}
 </script>
 
 <h1 class="text-2xl font-bold">Home Page</h1>
@@ -24,9 +39,22 @@
 		Configurations for displaying self-managed links at the home page.
 	</p>
 
-	<div class="flex items-center gap-3">
-		<Switch id="beta-features" bind:checked={isEnableBetaFeatures} />
+	<div class="mb-6 flex items-center gap-3">
+		<Switch id="beta-features" bind:checked={isLinkEnabled} />
 		<Label.Root for="beta-features">Enable Links</Label.Root>
+	</div>
+
+	<div>
+		<Label.Root class="mb-3" for="port">Display Count</Label.Root>
+		<Input
+			name="clipboard-count"
+			placeholder="e.g. 30"
+			min={0}
+			max={30}
+			bind:value={linkCount}
+			type="number"
+			class="mb-4"
+		/>
 	</div>
 
 	<Separator class="my-6 max-w-sm" />
@@ -37,14 +65,14 @@
 	</p>
 
 	<div class="mb-8 flex items-center gap-3">
-		<Switch id="beta-features" bind:checked={isEnableBetaFeatures} />
+		<Switch id="beta-features" bind:checked={isWebSearchEnabled} />
 		<Label.Root for="beta-features">Enable Web Search</Label.Root>
 	</div>
 
 	<Label.Root for="searchEngine" class="text-foreground mb-3">Default search engine</Label.Root>
-	<Select.Root type="single" name="searchEngine" bind:value>
+	<Select.Root type="single" name="searchEngine" bind:value={searchEngineInput}>
 		<Select.Trigger class="w-full">
-			{value || 'Select an engine'}
+			{searchEngineInput || 'Select an engine'}
 		</Select.Trigger>
 		<Select.Content>
 			<Select.Group>
@@ -57,4 +85,10 @@
 			</Select.Group>
 		</Select.Content>
 	</Select.Root>
+
+	<Separator class="my-6 " />
+
+	<div class="grid w-full gap-4">
+		<Button size="sm" variant="outline" onclick={saveChanges}>Save Changes</Button>
+	</div>
 </section>
