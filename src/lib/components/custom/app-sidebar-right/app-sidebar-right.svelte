16ab93d325842937a09175b5feb8cd21c5e3bcf9
Shaun Chong <data-miner00@users.noreply.github.com>
2026-06-20 17:05:02 +0800

    feat: View calendar button

--- src/lib/components/custom/app-sidebar-right/app-sidebar-right.svelte
+++ src/lib/components/custom/app-sidebar-right/app-sidebar-right.svelte
@@ -1,7 +1,8 @@
 <script lang="ts">
-	import { PencilIcon } from '@lucide/svelte';
+	import { CalendarIcon, PencilIcon } from '@lucide/svelte';
 	import type { ComponentProps } from 'svelte';
 
+	import { goto } from '$app/navigation';
 	import * as Sidebar from '$lib/components/ui/sidebar/index.js';
 
 	import DatePicker from './date-picker.svelte';
@@ -22,6 +23,15 @@
 	</Sidebar.Header>
 	<Sidebar.Content>
 		<DatePicker />
+		<Sidebar.Menu class="px-2">
+			<Sidebar.MenuItem>
+				<Sidebar.MenuButton onclick={() => goto('/tools/calendar')}>
+					<CalendarIcon />
+					<span>View Calendar</span>
+				</Sidebar.MenuButton>
+			</Sidebar.MenuItem>
+		</Sidebar.Menu>
+
 		<Sidebar.Separator class="mx-0" />
 		<Note />
 	</Sidebar.Content>
